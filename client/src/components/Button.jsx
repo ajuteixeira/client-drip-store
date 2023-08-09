8b4b4be133c627bad146fbb8c631a0297bf2779b
Juliana Teixeira <teixeirajuuh@gmail.com>
2023-08-08 22:58:13 -0300

    estilo desabilitado no componente botão

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -5,17 +5,17 @@ export default function Button(props) {
 
     ${
       props.color === 'pink' &&
-      'text-[#f5f5f5] bg-[#C92071] hover:bg-[#991956] disabled:bg-[#CCCCCC]'
+      'text-[#f5f5f5] bg-[#C92071] hover:bg-[#991956] disabled:bg-[#CCCCCC] disabled:cursor-not-allowed'
     }
 
     ${
       props.color === 'white' &&
-      'text-[#C92071] bg-[#F5F5F5] hover:bg-[#B5B6F2] hover:text-[#F5F5F5] disabled:opacity-50 disabled:border disabled:border-[#C92071]'
+      'text-[#C92071] bg-[#F5F5F5] hover:bg-[#B5B6F2] hover:text-[#F5F5F5] disabled:opacity-50 disabled:border disabled:border-[#C92071] disabled:cursor-not-allowed'
     }
 
     ${
       props.color === 'yellow' &&
-      'text-[#f5f5f5] bg-[#FFB31F] hover:bg-[#CF8900] disabled:bg-[#CCCCCC]'
+      'text-[#f5f5f5] bg-[#FFB31F] hover:bg-[#CF8900] disabled:bg-[#CCCCCC] disabled:cursor-not-allowed'
     }
 
     ${

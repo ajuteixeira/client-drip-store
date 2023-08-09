73bbbcc098671f2561630333672306d3ffa1cbde
deborassilva <debora-santos-1@hotmail.com>
2023-08-09 09:38:03 -0300

    criação do logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <Header />
     </>
   );
 }
---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,8 @@
+import logo from "../assest/img/Group.png"
+
+export default function Logo() {
+    return(
+        <img src={logo} alt="logo" title="Digital college"/> 
+    
+    )
+}

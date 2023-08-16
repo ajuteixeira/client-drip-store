30e2acb324307d289ada16a8ead4ac962e0c1c9d
FabioJrEvo <127104713+FabioJrEvo@users.noreply.github.com>
2023-08-16 10:14:09 -0300

    navbar adicionado ao header

--- client/src/pages/UiView.jsx
+++ client/src/pages/UiView.jsx
@@ -1,13 +1,15 @@
-import Navbar from "../components/Navbar";
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import CollectionCards from '../components/CollectionCards';
 import TrendingProducts from '../components/TrendingProducts';
+import Header from "../components/Header";
+import NavbarItem from '../components/NavbarItem';
 
 export default function UiTest() {
   return (
     <main className="min-w-full">
-      <Navbar/>
+      <Header/>
+      <NavbarItem/>
       <Hero />
       <CollectionCards />
       <TrendingProducts />

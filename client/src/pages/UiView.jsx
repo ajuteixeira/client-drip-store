498eedfdf0041ab63754d42e76b6db2a783369eb
ajuteixeira <teixeirajuuh@gmail.com>
2023-08-16 08:59:01 -0300

    ajustes

--- client/src/pages/UiView.jsx
+++ client/src/pages/UiView.jsx
@@ -1,17 +1,17 @@
-import Footer from "../components/Footer";
-import Footericons from "../components/FooterIcons";
+import Footer from '../components/Footer';
+import Footericons from '../components/FooterIcons';
 import Hero from '../components/Hero';
 import CollectionCards from '../components/CollectionCards';
 import TrendingProducts from '../components/TrendingProducts';
 
 export default function UiTest() {
-  return( 
+  return (
     <main className="min-w-full">
-     <Footer />
-     <Footericons />
-     <CollectionCards />
-     <Hero />
-     <TrendingProducts />
-   </main>;
-);
+      <Footer />
+      <Footericons />
+      <CollectionCards />
+      <Hero />
+      <TrendingProducts />
+    </main>
+  );
 }

0345912ab8b33394897d0eaaa8c7e60062201d42
luizreboucas <primoneto@gmail.com>
2023-08-30 08:55:37 -0300

    inicio modalMenu

--- client/src/components/ModalMenu.jsx
+++ client/src/components/ModalMenu.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import NavbarItem from './NavbarItem'
 
 const ModalMenu = (props) => {
   return (
-    <div className='h-48 w-48 bg-orange-500 absolute'>
+    <div className='h-48 w-48 bg-white absolute'>
+        <div>
+            <nav className={''}>
+            <div className={'gap-2 flex flex-col font-bold text-base'}>
+                <NavbarItem path="/" title="Home" />
+                <NavbarItem path="/produtos" title="Produtos" />
+                <NavbarItem path="/categorias" title="Categorias" />
+                <NavbarItem path="/pedidosroutes" title="Meus Pedidos" />
+            </div>
+            </nav>
+        </div>
       
     </div>
     
